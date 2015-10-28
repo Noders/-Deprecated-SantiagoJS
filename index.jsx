573b741998875fe0d2c5f8b5f2d5c91de2283d2f
fetorres <fetorres@groupon.com>
2015-10-28 00:05:54 -0300

    adding css-es

--- index.jsx
+++ index.jsx
@@ -1,23 +1,39 @@
-var React = require('react');
-require('./assets/css/bootstrap.styl');
+/*
 
-require('./assets/css/plugins/animate.styl');
 require('./assets/css/plugins/slick.styl');
 require('./assets/css/plugins/magnific-popup.styl');
-require('./assets/css/plugins/font-awesome.styl');
-require('./assets/css/plugins/streamline-icons.styl');
+require('./assets/css/plugins/streamline-icons.css');
 require('./assets/css/plugins/flexslider.styl');
 
-require('./assets/css/event.styl');
-require('./assets/themes/yellow.styl');
+require('./assets/css/event.css');
+require('./assets/css/themes/yellow.styl');
 
 require('./assets/js/modernizr.min.js');
 
 require('./assets/js/plugins/pace.js');
 require('./assets/js/plugins/google-map.js');
 
-require('./assets/js/jquery.min.js');
-require('./assets/js/bootstrap.min.js');
+require('./assets/js/main.js');
+*/
+
+require('font-awesome-webpack');
+window.$ = window.jQuery = require('jquery/dist/jquery.min.js');
+require('bootstrap-webpack');
+require('font-awesome-webpack');
+require('animate.css/animate.min.css')
+
+require('./assets/js/modernizr.min.js');
+require('./assets/css/plugins/slick.styl');
+require('./assets/css/plugins/magnific-popup.styl');
+require('./assets/css/plugins/streamline-icons.css');
+require('./assets/css/plugins/flexslider.styl');
+require('./assets/css/event.css');
+require('./assets/css/themes/yellow.styl');
+
+require('./assets/js/modernizr.min.js');
+require('./assets/js/plugins/pace.js');
+require('./assets/js/plugins/google-map.js');
 require('./assets/js/main.js');
 
+var React = require('react');
 React.render((require('./router.config.jsx')), document.body);
