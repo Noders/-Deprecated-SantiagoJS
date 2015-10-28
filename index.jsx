49c4690d8c2dde977317473ad86a4ffeac9effab
fetorres <fetorres@groupon.com>
2015-10-28 00:08:28 -0300

    fixing dependencies

--- index.jsx
+++ index.jsx
@@ -1,21 +1,3 @@
-/*
-
-require('./assets/css/plugins/slick.styl');
-require('./assets/css/plugins/magnific-popup.styl');
-require('./assets/css/plugins/streamline-icons.css');
-require('./assets/css/plugins/flexslider.styl');
-
-require('./assets/css/event.css');
-require('./assets/css/themes/yellow.styl');
-
-require('./assets/js/modernizr.min.js');
-
-require('./assets/js/plugins/pace.js');
-require('./assets/js/plugins/google-map.js');
-
-require('./assets/js/main.js');
-*/
-
 require('font-awesome-webpack');
 window.$ = window.jQuery = require('jquery/dist/jquery.min.js');
 require('bootstrap-webpack');
