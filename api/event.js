bd3cec8c4f8d859f9b118f6f534ea4b85c705ca5
fetorres <fetorres@groupon.com>
2015-11-05 18:04:15 -0300

    fixing event.js api images size

--- api/event.js
+++ api/event.js
@@ -3,7 +3,7 @@ module.exports = {
     date:'11/11/2015',
     speakers:[{
         "name": "Felipe Torres",
-        "img": "https://placehold.it/300?text=Speaker+1",
+        "img": "https://placehold.it/130?text=Speaker+1",
         "talk": {
             "title": "Charla sobre tema xxx xx xxxx xx",
             "description": "Aprende sobre xxx xx xxxx xx de manera cool y entretenida y blah blah blah en 20 minutos"
@@ -14,7 +14,7 @@ module.exports = {
         }]
     }, {
         "name": "Fernando Larrañaga",
-        "img": "https://placehold.it/300?text=Speaker+2",
+        "img": "https://placehold.it/130?text=Speaker+2",
         "talk": {
             "title": "Charla sobre tema yy yyy yy yyyy",
             "description": "Aprende sobre yy yyy yy yyyy de manera cool y entretenida y blah blah blah en 20 minutos"
@@ -25,7 +25,7 @@ module.exports = {
         }]
     }, {
         "name": "Estiv Llobs",
-        "img": "https://placehold.it/300?text=Speaker+3",
+        "img": "https://placehold.it/130?text=Speaker+3",
         "talk": {
             "title": "Charla sobre tema yy yyy yy yyyy",
             "description": "Aprende sobre yy yyy yy yyyy de manera cool y entretenida y blah blah blah en 20 minutos"
