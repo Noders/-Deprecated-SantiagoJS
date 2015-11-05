841a0af651573a3009e786210d81108ca58ccde1
Fernando Larrañaga <xabadu@gmail.com>
2015-11-05 13:28:43 -0300

    Translate texts

--- components/subscriptions/subscriptions.jsx
+++ components/subscriptions/subscriptions.jsx
@@ -6,17 +6,17 @@ module.exports = React.createClass({
 			<div className="highlight  col-xs-12 no-padding-v">
 		        <div className="container">
 		            <div className="row">
-		                <form action="php/subscribe.php" method="post" className="form subscribe-form" id="subscribeform">
+		                <form action="" method="post" className="form subscribe-form" id="subscribeform">
 		                    <div className="form-group col-md-3 hidden-sm">
-		                        <h6 className="susbcribe-head wow fadeInLeft"> SUBSCRIBE <small>TO OUR NEWSLETTER</small></h6>
+		                        <h6 className="susbcribe-head wow fadeInLeft"> SUSCRÍBETE <small>A NUESTROS NEWSLETTERS</small></h6>
 		                    </div>
 		                    <div className="form-group col-sm-8 col-md-6 wow fadeInRight">
-		                        <label className="sr-only">Email address</label>
-		                        <input type="email" className="form-control input-lg" placeholder="Enter your email" name="email" id="email" required />
-		                        <div id="js-subscribe-result" className="text-center" data-success-msg="Almost finished. Please check your email and verify." data-error-msg="Oops. Something went wrong."></div>
+		                        <label className="sr-only">Tu correo</label>
+		                        <input type="email" className="form-control input-lg" placeholder="Tu email" name="email" id="email" required />
+		                        <div id="js-subscribe-result" className="text-center" data-success-msg="Casi listos. Revisa tu correo para confirmar." data-error-msg="Oops. Something went wrong."></div>
 		                    </div>
 		                    <div className="form-group col-sm-4 col-md-3">
-		                        <button type="submit" className="btn btn-lg btn-success btn-block" id="js-subscribe-btn">Subscribe Now →</button>
+		                        <button type="submit" className="btn btn-lg btn-success btn-block" id="js-subscribe-btn">Suscribirse →</button>
 		                    </div>
 		                </form>
 		            </div>
