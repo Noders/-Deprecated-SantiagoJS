0cc686e695504d910221cbfa19f31e5284b15ec6
Fernando Larrañaga <xabadu@gmail.com>
2016-03-03 23:57:21 -0300

    Update event info

--- api/event.js
+++ api/event.js
@@ -1,41 +1,41 @@
 module.exports = {
-  url: 'http://www.meetup.com/es-ES/Javascript-Chile/events/227777549/',
-  date: '2/4/2016',
+  url: 'http://www.meetup.com/es-ES/Javascript-Chile/events/228628694/',
+  date: '3/10/2016',
   speakers: [{
-    'name': 'Cesar Encina Leon',
-    'img': 'https://avatars1.githubusercontent.com/u/5914439?v=3&s=230',
+    'name': 'Raúl Pino',
+    'img': 'https://avatars0.githubusercontent.com/u/779581?v=3&s=230',
     'talk': {
-      'title': {__html: '"Ahorrándose" el BackEnd'},
-      'description': 'Implementación de una galería de imágenes dinámica con github pages + AWS Lambda + Firebase + AWS S3 y un poquito de AngularJS'
+      'title': {__html: 'A complete distributed relay system in JS'},
+      'description': 'The main focus of this talk will be explore the challenges of developing a mobile app using Ionic and a web app using Express, ultimately supporting communication between phones via BLE/SMS, and a central server via HTTP. Messages will spread on this distributed phone network until they reach the central server; from there, they will be relayed to targeted social networks.'
     },
     'social': [{
       'type': 'twitter',
-      'url': 'https://twitter.com/DMaster69'
+      'url': 'https://twitter.com/p1nox'
     }, {
       'type': 'github',
-      'url': 'https://github.com/DMaster69'
+      'url': 'https://github.com/p1nox'
     }]
   }, {
-    'name': 'Ricardo Campos',
-    'img': 'https://avatars1.githubusercontent.com/u/5192990?v=3&s=230',
+    'name': 'N. Riesco',
+    'img': 'https://avatars2.githubusercontent.com/u/1995514?v=3&s=230',
     'talk': {
-      'title': {__html: 'Ionic + Firebase: "De la idea al prototipo en un día"'},
-      'description': 'Conjunto de mockups funcionales con (Ionic + Ionic Creator) y un backend con (Firebase + Angularfire) y actualizaciones en tiempo real de datos'
+      'title': {__html: 'Aplicar agilidad en tu empresa: Tests unitarios con Mocha'},
+      'description': 'Esto permite trabajar en forma segura, aplicando BDD/TDD con automatización a un 100% Por otra parte permite reutilizar fácilmente las librerías y tener un control absoluto de sus versiones.'
     },
     'social': [{
       'type': 'github',
-      'url': 'https://github.com/rcamposp'
+      'url': 'https://github.com/nriesco'
     }]
   }, {
-    'name': '¿Tu?',
-    'img': '../assets/images/javascript.png',
+    'name': 'Carlos Betancourt',
+    'img': 'https://avatars3.githubusercontent.com/u/324564?v=3&s=230',
     'talk': {
-      'title': {__html: '<a href="http://github.com/JavascriptChile/Meetups">¡Inscribe tu charla en nuestro GitHub!</a>'},
-      'description': 'Conjunto de mockups funcionales con (Ionic + Ionic Creator) y un backend con (Firebase + Angularfire) y actualizaciones en tiempo real de datos'
+      'title': {__html: 'Dynamic client-side PDF generation'},
+      'description': 'I want to talk about uBiomes use case of dynamic client-side PDF generation (our in-house reports microservice): reasons, approach taken, tools, challenges, pros and cons, and benefits for stakeholders.'
     },
     'social': [{
       'type': 'github',
-      'url': 'https://github.com/JavascriptChile'
+      'url': 'https://github.com/betacar'
     }]
   }]
 }
