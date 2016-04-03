5f6adec833469c5a718f2579d21ff9c2e06afdf9
Fernando Larrañaga <xabadu@gmail.com>
2016-04-03 14:07:26 -0300

    gh-pages update

--- api/event.js
+++ api/event.js
@@ -1,41 +1,41 @@
 module.exports = {
-  url: 'http://www.meetup.com/es-ES/Javascript-Chile/events/228628694/',
-  date: '3/10/2016',
+  url: 'http://www.meetup.com/es-ES/Javascript-Chile/events/228936577/',
+  date: '4/07/2016',
   speakers: [{
-    'name': 'Raúl Pino',
-    'img': 'https://avatars0.githubusercontent.com/u/779581?v=3&s=230',
+    'name': 'Felipe Torres',
+    'img': 'https://avatars0.githubusercontent.com/u/952992?v=3&s=230',
     'talk': {
-      'title': {__html: 'A complete distributed relay system in JS'},
+      'title': {__html: 'PAMBot: Node + Telegram = Happy Partners'},
       'description': 'The main focus of this talk will be explore the challenges of developing a mobile app using Ionic and a web app using Express, ultimately supporting communication between phones via BLE/SMS, and a central server via HTTP. Messages will spread on this distributed phone network until they reach the central server; from there, they will be relayed to targeted social networks.'
     },
     'social': [{
       'type': 'twitter',
-      'url': 'https://twitter.com/p1nox'
+      'url': 'https://twitter.com/fforres'
     }, {
       'type': 'github',
-      'url': 'https://github.com/p1nox'
+      'url': 'https://github.com/fforres'
     }]
   }, {
-    'name': 'N. Riesco',
-    'img': 'https://avatars2.githubusercontent.com/u/1995514?v=3&s=230',
+    'name': 'Juan Pizarro',
+    'img': 'https://avatars2.githubusercontent.com/u/111236?v=3&s=230',
     'talk': {
-      'title': {__html: 'Aplicar agilidad en tu empresa: Tests unitarios con Mocha'},
-      'description': 'Esto permite trabajar en forma segura, aplicando BDD/TDD con automatización a un 100% Por otra parte permite reutilizar fácilmente las librerías y tener un control absoluto de sus versiones.'
+      'title': {__html: 'Event-driven Code with Node.js in AWS Lambda'},
+      'description': 'Mostraré una implementación de notificaciones de nuevas licitaciones usando datos de la api de mercado publico con AWS Lambda + Amazon DynamoDB + Amazon Kinesis + Amazon SNS.'
     },
     'social': [{
       'type': 'github',
-      'url': 'https://github.com/nriesco'
+      'url': 'https://github.com/jpizarrom'
     }]
   }, {
-    'name': 'Carlos Betancourt',
-    'img': 'https://avatars3.githubusercontent.com/u/324564?v=3&s=230',
+    'name': 'Fernando Larrañaga',
+    'img': 'https://avatars3.githubusercontent.com/u/3161019?v=3&s=230',
     'talk': {
-      'title': {__html: 'Dynamic client-side PDF generation'},
-      'description': 'I want to talk about uBiomes use case of dynamic client-side PDF generation (our in-house reports microservice): reasons, approach taken, tools, challenges, pros and cons, and benefits for stakeholders.'
+      'title': {__html: 'NodeSchool Santiago: En Stereo, donde esté disponible.'},
+      'description': 'How we put together NodeSchool Santiago and what plans we have for the future.'
     },
     'social': [{
       'type': 'github',
-      'url': 'https://github.com/betacar'
+      'url': 'https://github.com/xabadu'
     }]
   }]
 }
