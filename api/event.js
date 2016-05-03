9ba87f154c4f03922c913acf92162d4aeae62ec1
Fernando Larrañaga <xabadu@gmail.com>
2016-05-03 20:02:16 -0300

    Event updated

--- api/event.js
+++ api/event.js
@@ -1,25 +1,25 @@
 module.exports = {
-  url: 'http://www.meetup.com/es-ES/Javascript-Chile/events/228936577/',
-  date: '4/07/2016',
+  url: 'http://www.meetup.com/es/Javascript-Chile/events/229473031/',
+  date: '5/05/2016',
   speakers: [{
-    'name': 'Felipe Torres',
-    'img': 'https://avatars0.githubusercontent.com/u/952992?v=3&s=230',
+    'name': 'Camilo Chacón',
+    'img': 'https://avatars0.githubusercontent.com/u/1832594?v=3&s=230',
     'talk': {
-      'title': {__html: 'PAMBot: Node + Telegram = Happy Partners'},
+      'title': {__html: 'Introducción a ZeroMQ y Node.js: Para construir sistemas distribuidos de mensajería.'},
       'description': 'The main focus of this talk will be explore the challenges of developing a mobile app using Ionic and a web app using Express, ultimately supporting communication between phones via BLE/SMS, and a central server via HTTP. Messages will spread on this distributed phone network until they reach the central server; from there, they will be relayed to targeted social networks.'
     },
     'social': [{
       'type': 'twitter',
-      'url': 'https://twitter.com/fforres'
+      'url': 'https://twitter.com/camilochs'
     }, {
       'type': 'github',
-      'url': 'https://github.com/fforres'
+      'url': 'https://github.com/camilochs'
     }]
   }, {
-    'name': 'Juan Pizarro',
-    'img': 'https://avatars2.githubusercontent.com/u/111236?v=3&s=230',
+    'name': 'Thiago Costa',
+    'img': 'https://avatars2.githubusercontent.com/u/3903371?v=3&s=230',
     'talk': {
-      'title': {__html: 'Event-driven Code with Node.js in AWS Lambda'},
+      'title': {__html: 'ScalaJS: Boom, there it is.'},
       'description': 'Mostraré una implementación de notificaciones de nuevas licitaciones usando datos de la api de mercado publico con AWS Lambda + Amazon DynamoDB + Amazon Kinesis + Amazon SNS.'
     },
     'social': [{
